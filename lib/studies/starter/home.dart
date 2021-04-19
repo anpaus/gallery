422518016a8db026f348d701db83855f13b5d4b4
Pawel <info@anpa.us>
2021-04-19 17:39:30 +0200

    Demos

--- lib/studies/starter/home.dart
+++ lib/studies/starter/home.dart
@@ -8,7 +8,58 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
 import 'package:gallery/layout/adaptive.dart';
 
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+
 const appBarDesktopHeight = 128.0;
+Future<Album> futureAlbum;
+int selectedItem = 0;
+
+Future<Album> fetchAlbum(String i) async {
+  final String s = 'jsonplaceholder.typicode.com';
+  final String a = 'albums/' + i;
+  final response = await http.get(Uri.https(s,a));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album ' + s + a);
+  }
+}
+
+class AlbumModel extends ChangeNotifier {
+  void refresh() {
+    notifyListeners();
+  }
+}
+
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({@required this.userId, @required this.id, @required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+
+  String getTitle() => title;
+}
+
+
 
 class HomePage extends StatelessWidget {
   const HomePage();
@@ -18,6 +69,7 @@ class HomePage extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
     final isDesktop = isDisplayDesktop(context);
+
     final body = SafeArea(
       child: Padding(
         padding: isDesktop
@@ -27,10 +79,11 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              GalleryLocalizations.of(context).starterAppGenericHeadline,
+              /*GalleryLocalizations.of(context).starterAppGenericHeadline*/ selectedItem.toString(),
               style: textTheme.headline3.copyWith(
                 color: colorScheme.onSecondary,
               ),
+
             ),
             const SizedBox(height: 10),
             Text(
@@ -42,13 +95,14 @@ class HomePage extends StatelessWidget {
               GalleryLocalizations.of(context).starterAppGenericBody,
               style: textTheme.bodyText1,
             ),
+
           ],
         ),
       ),
     );
 
     if (isDesktop) {
-      return Row(
+      /*return Row(
         children: [
           ListDrawer(),
           const VerticalDivider(width: 1),
@@ -57,7 +111,7 @@ class HomePage extends StatelessWidget {
               appBar: const AdaptiveAppBar(
                 isDesktop: true,
               ),
-              body: body,
+              body: BodyDynamic(),
               floatingActionButton: FloatingActionButton.extended(
                 heroTag: 'Extended Add',
                 onPressed: () {},
@@ -71,11 +125,44 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
+      );*/
+      return ChangeNotifierProvider(
+        create: (context) => AlbumModel(),
+        child: Row(
+          children: [
+            Consumer<AlbumModel>(
+                builder: (context, cart, child) {
+                  return ListDrawer();
+                }),
+            const VerticalDivider(width: 1),
+            Expanded(
+              child: Scaffold(
+                appBar: const AdaptiveAppBar(
+                  isDesktop: true,
+                ),
+                body: Consumer<AlbumModel>(
+                  builder: (context, cart, child) {
+                    return BodyDynamic();
+                  }),
+                floatingActionButton: FloatingActionButton.extended(
+                  heroTag: 'Extended Add',
+                  onPressed: () {},
+                  label: Text(
+                    GalleryLocalizations.of(context).starterAppGenericButton,
+                    style: TextStyle(color: colorScheme.onSecondary),
+                  ),
+                  icon: Icon(Icons.add, color: colorScheme.onSecondary),
+                  tooltip: GalleryLocalizations.of(context).starterAppTooltipAdd,
+                ),
+              ),
+            ),
+          ],
+        ),
       );
     } else {
-      return Scaffold(
+      /*return Scaffold(
         appBar: const AdaptiveAppBar(),
-        body: body,
+        body: BodyDynamic(),
         drawer: ListDrawer(),
         floatingActionButton: FloatingActionButton(
           heroTag: 'Add',
@@ -86,6 +173,29 @@ class HomePage extends StatelessWidget {
             color: Theme.of(context).colorScheme.onSecondary,
           ),
         ),
+      );*/
+      return ChangeNotifierProvider(
+        create: (context) => AlbumModel(),
+        child: Scaffold(
+          appBar: const AdaptiveAppBar(),
+          body: Consumer<AlbumModel>(
+              builder: (context, cart, child) {
+                return BodyDynamic();
+              }),
+          drawer: Consumer<AlbumModel>(
+              builder: (context, cart, child) {
+                return ListDrawer();
+              }),
+          floatingActionButton: FloatingActionButton(
+          heroTag: 'Add',
+          onPressed: () {},
+          tooltip: GalleryLocalizations.of(context).starterAppTooltipAdd,
+          child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.onSecondary,
+          ),
+          ),
+          ),
       );
     }
   }
@@ -154,9 +264,17 @@ class ListDrawer extends StatefulWidget {
 }
 
 class _ListDrawerState extends State<ListDrawer> {
+
   static final numItems = 9;
 
-  int selectedItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum(selectedItem.toString());
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -176,21 +294,98 @@ class _ListDrawerState extends State<ListDrawer> {
               ),
             ),
             const Divider(),
+
             ...Iterable<int>.generate(numItems).toList().map((i) {
               return ListTile(
                 enabled: true,
                 selected: i == selectedItem,
                 leading: const Icon(Icons.favorite),
-                title: Text(
-                  GalleryLocalizations.of(context).starterAppDrawerItem(i + 1),
+                title: FutureBuilder<Album>(
+                  future: futureAlbum,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data.title);
+                    } else if (snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+
+                    // By default, show a loading spinner.
+                    return Text("Loading...");
+                  },
                 ),
                 onTap: () {
                   setState(() {
                     selectedItem = i;
+                    futureAlbum = fetchAlbum(selectedItem.toString());
+                    var cart = context.read<AlbumModel>();
+                    cart.refresh();
                   });
                 },
               );
             }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class BodyDynamic extends StatefulWidget {
+  @override
+  _BodyDynamicState createState() => _BodyDynamicState();
+}
+
+class _BodyDynamicState extends State<BodyDynamic> {
+
+  static final numItems = 9;
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    final isDesktop = isDisplayDesktop(context);
+    return SafeArea(
+      child: Padding(
+        padding: isDesktop
+            ? const EdgeInsets.symmetric(horizontal: 72, vertical: 48)
+            : const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              /*GalleryLocalizations.of(context).starterAppGenericHeadline*/ selectedItem.toString(),
+              style: textTheme.headline3.copyWith(
+                color: colorScheme.onSecondary,
+              ),
+
+            ),
+            const SizedBox(height: 10),
+            Text(
+              GalleryLocalizations.of(context).starterAppGenericSubtitle,
+              style: textTheme.subtitle1,
+            ),
+            const SizedBox(height: 48),
+            FutureBuilder<Album>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.title);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                // By default, show a loading spinner.
+                return Text("Loading...");
+              },
+            ),
+
           ],
         ),
       ),
